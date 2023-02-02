3762dd1b544fedf36d520f8f4afe9b8d47be852e
Latumi1192 <honghai.bui@haw-hamburg.de>
2023-02-02 13:46:03 +0100

    Added PhotoSlide without function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import GuestForm from "@/features/presentation/components/GuestForm";
 import PageBar from "@/features/presentation/components/PageBar";
+import PhotoSlide from "@/features/presentation/components/PhotoSlide";
 
 const Home = () => {
-  return <GuestForm />;
+  return <PhotoSlide />;
 };
 
 export default Home;
