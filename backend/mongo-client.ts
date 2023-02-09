bfca1a7c81f36b7a7f1c462e580017dc4ea9bec4
Latumi1192 <honghai.bui@haw-hamburg.de>
2023-02-09 15:52:53 +0100

    disable eslint

--- backend/mongo-client.ts
+++ backend/mongo-client.ts
@@ -1,4 +1,5 @@
-import { MongoClient } from 'mongodb';
+/* eslint-disable */
+import { MongoClient } from "mongodb";
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid environment variable: "MONGODB_URI"');
@@ -11,10 +12,10 @@ let client;
 let clientPromise: Promise<MongoClient>;
 
 if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your Mongo URI to .env.local');
+  throw new Error("Please add your Mongo URI to .env.local");
 }
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!global._mongoClientPromise) {
