256a30105477465cdbfbb0d5304377c2186b0d14
Quang Duy Tran <hello.itctran@gmail.com>
2023-02-09 16:04:32 +0100

    fixed var type

--- backend/mongo-client.ts
+++ backend/mongo-client.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid environment variable: "MONGODB_URI"');
@@ -11,11 +11,17 @@ const options = {};
 let client;
 let clientPromise: Promise<MongoClient>;
 
-if (!process.env.MONGODB_URI) {
-  throw new Error("Please add your Mongo URI to .env.local");
+declare global {
+  namespace globalThis {
+    var _mongoClientPromise: Promise<MongoClient>;
+  }
 }
 
-if (process.env.NODE_ENV === "development") {
+if (!process.env.MONGODB_URI) {
+  throw new Error('Please add your Mongo URI to .env.local');
+}
+
+if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!global._mongoClientPromise) {
